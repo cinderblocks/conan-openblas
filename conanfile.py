238f03fae0018147dca0760e874fae3195fea7fa
Cinder Biscuits <cinder@cinderblocks.biz>
2017-09-29 06:10:22 -0500

    Add NASM as build requirement, fall back to v0.2.20 again

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class OpenBLASConan(ConanFile):
     name = "OpenBLAS"
-    version = "0.3.0"
+    version = "0.2.20"
     license = "https://github.com/xianyi/OpenBLAS/blob/develop/LICENSE"
     url = "https://github.com/cinderblocks/conan-openblas"
     description = "OpenBLAS is an optimized BLAS library based on GotoBLAS2 1.13 BSD version."
@@ -14,8 +14,7 @@ class OpenBLASConan(ConanFile):
 
     def source(self):
         self.run("git clone https://github.com/xianyi/OpenBLAS.git")
-        # Why this commit? Completely arbitrary. This was HEAD when I wrote this.
-        self.run("cd OpenBLAS && git checkout 00c42dc8155cabc0b28443bd647d2c268fd98bab")
+        self.run("cd OpenBLAS && git checkout tags/v0.2.20")
         # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
         # if the packaged project doesn't have variables to set it properly
         tools.replace_in_file("OpenBLAS/CMakeLists.txt", "project(OpenBLAS)", '''project(OpenBLAS)
